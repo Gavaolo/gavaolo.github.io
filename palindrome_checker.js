273a355709e879eac411c2b2d2eaa85443c2ff22
RokuoGun <82797405+RokuoGun@users.noreply.github.com>
2022-03-17 23:16:16 +0100

    Create palindrome_checker.js

---
+++ palindrome_checker.js
@@ -0,0 +1,16 @@
+function arrAreEqual(arr1,arr2){
+  for(let i=0;i<arr1.length;i++)
+    if(arr1[i]!=arr2[i]) return false
+
+  return arr1.length==arr2.length  
+}
+
+function palindrome(str) {
+
+  let regExp1 = /([a-z]|[0-9])/gi
+  
+  let myStrArr=str.toLowerCase().match(regExp1)
+  let myInvertedStrArr = str.toLowerCase().match(regExp1).reverse()
+
+  return arrAreEqual(myStrArr,myInvertedStrArr)
+}
