d712a75a7b18b0cbdc9262307de32247bfe12689
RokuoGun <gavassopaolo@gmail.com>
2022-03-19 18:14:43 +0100

    breakout

--- breakout/breakout.js
+++ breakout/breakout.js
@@ -1,13 +1,23 @@
 const grid = document.querySelector(".grid")
+const scoreDisplay = document.querySelector("#score")
 const blockWidth = 100
 const blockHeight = 20
 const xGap = 110
 const yGap = 30
-const boardWidth = grid.offsetWidth
+const boardWidth = grid.offsetWidth-1
+const boardHeight = grid.offsetHeight-1
+const userXMove = 20
+const ballSpeed = 20
+let timerId
+let xDirection = 2
+let yDirection = 2
+let score = 0
 
-const userStart = [230, 10]
-let currentPosition = userStart
+const userStartPosition = [230, 10]
+let userCurrentPosition = userStartPosition
 
+const ballStartPosition = [270, 40]
+let ballCurrentPosition = ballStartPosition
 //create block
 class Block {
     constructor(xAxis, yAxis) {
@@ -64,24 +74,108 @@ grid.appendChild(user)
 
 //darw user
 function drawUser() {
-    currentPosition[0] = Math.min(Math.max(currentPosition[0], 10), boardWidth-blockWidth-10)
-    user.style.left = currentPosition[0] + "px"
-    user.style.bottom = currentPosition[1] + "px" 
+    userCurrentPosition[0] = Math.min(Math.max(userCurrentPosition[0], 10), boardWidth-blockWidth-10)
+    user.style.left = userCurrentPosition[0] + "px"
+    user.style.bottom = userCurrentPosition[1] + "px" 
+}
+
+//draw ball
+function drawBall() {
+    ballCurrentPosition[0] = Math.min(Math.max(ballCurrentPosition[0], 0), boardWidth-ball.offsetWidth)
+    ballCurrentPosition[1] = Math.min(Math.max(ballCurrentPosition[1], 0), boardHeight-ball.offsetHeight)
+    ball.style.left = ballCurrentPosition[0] + "px"
+    ball.style.bottom = ballCurrentPosition[1] + "px" 
 }
 
 //move user
 function moveUser(e) {
     switch(e.key) {
         case "ArrowLeft":
-            currentPosition[0] -= 10
+            userCurrentPosition[0] -= userXMove
             drawUser()
             break;
         
         case "ArrowRight":
-            currentPosition[0] += 10
+            userCurrentPosition[0] += userXMove
             drawUser()
             break;
     }
 }
 
 document.addEventListener("keydown", moveUser)
+
+//add ball
+const ball = document.createElement("div")
+ball.classList.add("ball")
+drawBall()
+grid.appendChild(ball)
+
+//move ball
+function moveBall() {
+    ballCurrentPosition[0] += xDirection
+    ballCurrentPosition[1] += yDirection
+    drawBall()
+    checkForCollisions()
+}
+
+timerId = setInterval(moveBall, ballSpeed)
+
+//check for collisions
+function checkForCollisions() {
+    //check for block collisions
+    for (let i = 0; i < blocks.length; i++) {
+        
+        if (
+            (ballCurrentPosition[0] >= blocks[i].bottomLeft[0]) &&
+            (ballCurrentPosition[0] <= blocks[i].bottomRight[0]) &&
+            (ballCurrentPosition[1] + ball.offsetHeight > blocks[i].bottomLeft[1]) &&
+            (ballCurrentPosition[1] < blocks[i].topLeft[1])
+        ){
+            const allBlocks = Array.from(document.querySelectorAll(".block"))
+            allBlocks[i].classList.remove("block")
+            blocks.splice(i,1)
+            changeDirection()
+            score++
+            scoreDisplay.innerHTML = score
+        }
+
+        //check for win
+        if (blocks.length === 0) {
+            scoreDisplay.innerHTML = "You win"
+            clearInterval(timerId)
+            document.removeEventListener("keydown", moveUser)
+        }
+    }
+
+    //check for wall collisions
+    if (
+        (ballCurrentPosition[0] >= (boardWidth-ball.offsetWidth)) || 
+        (ballCurrentPosition[0] <= 0) || 
+        (ballCurrentPosition[1] >= (boardHeight-ball.offsetHeight))
+        ) {
+        changeDirection()
+    }
+
+    //check for user collision
+    if (ballCurrentPosition[0] > userCurrentPosition[0] && 
+        ballCurrentPosition[0] < userCurrentPosition[0] + blockWidth &&
+        ballCurrentPosition[1] > userCurrentPosition[1] &&
+        ballCurrentPosition[1] < userCurrentPosition[1] + blockHeight
+        ) {
+            changeDirection()
+        }
+
+    //check for game over
+    if (ballCurrentPosition[1] <= 0) {
+        scoreDisplay.innerHTML = "Game over"
+        clearInterval(timerId)
+        document.removeEventListener("keydown", moveUser)
+    }
+}
+
+function changeDirection() {
+    if ((xDirection > 0) && (yDirection > 0)) {yDirection = -yDirection; return}
+    if ((xDirection > 0) && (yDirection < 0)) {xDirection = -xDirection; return}
+    if ((xDirection < 0) && (yDirection < 0)) {yDirection = -yDirection; return}
+    if ((xDirection < 0) && (yDirection > 0)) {xDirection = -xDirection; return}
+}
