bce2283f8b9661a9ff9715abf522544808150539
RokuoGun <gavassopaolo@gmail.com>
2022-03-23 20:46:10 +0100

    mod

--- projects/games/connect_four/connect_four.js
+++ projects/games/connect_four/connect_four.js
@@ -11,18 +11,33 @@ let squares = []
 function createBoard() {
     board.style.width = boardWidth * squareWidth + "px"
     board.style.height = boardHeight * squareHeight + "px"
-    for (let i = 0; i < (boardWidth * boardHeight); i++) {
+    for (let i = 0; i < (boardWidth * (boardHeight + 1)); i++) {
         squares[i] = document.createElement("div")
         squares[i].classList.add("square")
         squares[i].style.width = squareWidth + "px"
         squares[i].style.height = squareHeight + "px"
-        if (i >= ((boardWidth * boardHeight) - boardWidth)) squares[i].classList.add("taken")
+        if (i >= (boardWidth * boardHeight)) squares[i].classList.add("taken")
         board.appendChild(squares[i])
     }
 }
 
-function checkBoard() {
+function checkUp(start, player) {
+    var cnt = 0
+    for (let i = start; i < 4; i += boardWidth) {
+        if (squares[i].classList.contains(player)) cnt++
+        console.log(squares[i] + " " + cnt);
+    }
+    if (cnt==4) console.log(player + " wins!");
+    
+}
 
+function checkBoard() {
+    for (let i = 0; i < (boardWidth * boardHeight); i++) {
+        if (squares[i].classList.contains("player-one")) {
+            checkUp(i, "player-one")
+        }
+        
+    }
 }
 
 createBoard()
@@ -45,6 +60,7 @@ squares.forEach((square, i) => {
         } else {
             alert("Can't go here")
         }
+        console.log(square);
         checkBoard()
     }    
 });
