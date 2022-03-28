200a7fe39c7c5707d350288d18990aa7e36d2848
RokuoGun <gavassopaolo@gmail.com>
2022-03-28 20:12:33 +0200

    reac_fun_facts

--- projects/page_template/react_facts/react_facts.js
+++ projects/page_template/react_facts/react_facts.js
@@ -1,7 +1,15 @@
-const app = 
-    <ul>List title
-        <li>List item 0</li>
-        <li>List item 1</li>
-    </ul>  
+const app = (
+    <div>
+        <img src="./img/react-logo.png" />
+        <h1>Fun facts about React</h1>
+        <ul>
+            <li>Was firtst released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K strars on GitHub</li>
+            <li>Is mantained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including moible apps</li>
+        </ul>
+    </div>
+)
 
 ReactDOM.render(app, document.querySelector("#root"))
