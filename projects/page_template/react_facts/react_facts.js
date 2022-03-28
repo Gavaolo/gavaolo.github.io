39a169a1112692b5f37167656e0eb820b57eb5a8
RokuoGun <gavassopaolo@gmail.com>
2022-03-28 18:52:18 +0200

    react

--- projects/page_template/react_facts/react_facts.js
+++ projects/page_template/react_facts/react_facts.js
@@ -1,1 +1,7 @@
-ReactDOM.render(<h1>Hello, everyone!</h1>, document.querySelector("#root"))
+const app = 
+    <ul>List title
+        <li>List item 0</li>
+        <li>List item 1</li>
+    </ul>  
+
+ReactDOM.render(app, document.querySelector("#root"))
