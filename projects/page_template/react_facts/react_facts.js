5447236166c86ccbd0de76817760ebfd437f362f
RokuoGun <gavassopaolo@gmail.com>
2022-03-28 21:56:07 +0200

    mod

--- projects/page_template/react_facts/react_facts.js
+++ projects/page_template/react_facts/react_facts.js
@@ -1,15 +1,43 @@
-const page = (
-    <div>
-        <img src="./img/react-logo.png" width="50px" />
-        <h1>Fun facts about React</h1>
-        <ul>
-            <li>Was firtst released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100K strars on GitHub</li>
-            <li>Is mantained by Facebook</li>
-            <li>Powers thousands of enterprise apps, including moible apps</li>
-        </ul>
-    </div>
-)
+function Header() {
+    return (
+        <header>
+            <nav>
+                <img src="./img/react-logo.png" width="40px" />
+            </nav>
+        </header>
+    )
+}
 
-ReactDOM.render(page, document.querySelector("#root"))
+function Footer() {
+    return (
+        <footer>
+            <small>© 2021 Ziroll development. All rights reserved.</small>
+        </footer>
+    )
+}
+
+function MainContent() {
+    return (
+        <div>
+            <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+                <li>It's a popular library, so I'll be 
+                able to fit in with the cool kids!</li>
+                <li>I'm more likely to get a job as a developer
+                if I know React</li>
+            </ol>
+        </div>
+    )
+}
+
+function Page() {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    )
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"))
