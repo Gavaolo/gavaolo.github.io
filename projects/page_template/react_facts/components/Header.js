0c0a4973cadb9b01cb9340b9eb027876097d5c40
RokuoGun <gavassopaolo@gmail.com>
2022-03-29 17:54:34 +0200

    mod

--- projects/page_template/react_facts/components/Header.js
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-    return (
-        <header>
-            <nav>
-                <img src="./img/react-logo.png" width="40px" />
-            </nav>
-        </header>
-    )
-}
--- projects/page_template/react_facts/react_facts.js
+++ projects/page_template/react_facts/react_facts.js
@@ -1,8 +1,34 @@
-import Header from ".\components\Header.js"
-import MainContent from ".\components\MainContent.js"
-import Footer from ".\components\Footer.js"
-// import react from "react"
-// import ReactDOM from "react-dom"
+function Header() {
+    return (
+        <header>
+            <nav>
+                <img src="./img/react-logo.png" width="40px" />
+            </nav>
+        </header>
+    )
+}
+
+function MainContent() {
+    return (
+        <div>
+            <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+                <li>It's a popular library, so I'll be 
+                able to fit in with the cool kids!</li>
+                <li>I'm more likely to get a job as a developer
+                if I know React</li>
+            </ol>
+        </div>
+    )
+}
+
+function Footer() {
+    return (
+        <footer>
+            <small>© 2021 Ziroll development. All rights reserved.</small>
+        </footer>
+    )
+}
 
 function App() {
     return (
