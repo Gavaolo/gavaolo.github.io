7d86a5cc565b2005f7db67846a25d975785b24cb
RokuoGun <gavassopaolo@gmail.com>
2022-03-29 17:15:53 +0200

    mod

--- projects/page_template/react_facts/components/MainContent.js
+++ projects/page_template/react_facts/components/MainContent.js
@@ -1,4 +1,4 @@
-function MainContent() {
+export default function MainContent() {
     return (
         <div>
             <h1>Reasons I'm excited to learn React</h1>
--- projects/page_template/react_facts/react_facts.js
+++ projects/page_template/react_facts/react_facts.js
@@ -1,9 +1,8 @@
-import Footer from ".\components\Footer.js"
 import Header from ".\components\Header.js"
 import MainContent from ".\components\MainContent.js"
-import react from "https://unpkg.com/react@17/umd/react.development.js"
-import ReactDOM from "react-dom"
-
+import Footer from ".\components\Footer.js"
+// import react from "react"
+// import ReactDOM from "react-dom"
 
 function App() {
     return (
