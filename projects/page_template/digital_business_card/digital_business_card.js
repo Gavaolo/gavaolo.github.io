213e88821b3cc0cfee9666c28ea2e23603f74b56
RokuoGun <gavassopaolo@gmail.com>
2022-03-29 22:11:14 +0200

    mod

---
+++ projects/page_template/digital_business_card/digital_business_card.js
@@ -0,0 +1,7 @@
+// https://www.figma.com/file/4ctPLUvIn5b5Ep6YPOZWWd/Digital-Business-Card?node-id=0%3A1
+
+// use separate components for:
+//     - info (photo, name, buttons)
+//     - about
+//     - interests
+//     - footer (social icons)
