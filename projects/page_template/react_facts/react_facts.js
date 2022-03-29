eb7132ae8967cdcba0374a21bf9bbc74f1813c2d
RokuoGun <gavassopaolo@gmail.com>
2022-03-29 21:59:34 +0200

    mod

--- projects/page_template/react_facts/react_facts.js
+++ projects/page_template/react_facts/react_facts.js
@@ -2,7 +2,7 @@ function Header() {
     return (
         <header>
             <nav>
-                <img src="./img/react-logo-small.png" className="nav-icon" />
+                <img src="./img/react-icon-small.png" className="nav-icon" />
                 <h3 className="nav-logo-text">ReactFacts</h3>
                 <h4 className="nav-title">React Course - Project 1</h4>
             </nav>
