0d5213671a458eeaacf4897df324cd4e1873ca46
RokuoGun <gavassopaolo@gmail.com>
2022-04-05 07:52:57 +0200

    mod

---
+++ projects/page_template/digital_business_card/Header.js
@@ -0,0 +1,11 @@
+
+export default function Header() {
+    return (
+        <header>
+            <img src="./img/profile_photo.jpg" />
+            <h1 className="card-name">Paolo Gavasso</h1>
+            <p className="job-description">Frontend Developer</p>
+            <small className="web-site"><a href="https://rokuogun.github.io/" target="_blank">ruokuogun.github.io</a></small>
+        </header>
+    )
+}
--- projects/page_template/digital_business_card/digital_business_card.js
+++ projects/page_template/digital_business_card/digital_business_card.js
@@ -1,13 +1,13 @@
-function Header() {
-    return (
-        <header>
-            <img src="./img/profile_photo.jpg" />
-            <h1 className="card-name">Paolo Gavasso</h1>
-            <p className="job-description">Frontend Developer</p>
-            <small className="web-site"><a href="https://rokuogun.github.io/" target="_blank">ruokuogun.github.io</a></small>
-        </header>
-    )
-}
+// function Header() {
+//     return (
+//         <header>
+//             <img src="./img/profile_photo.jpg" />
+//             <h1 className="card-name">Paolo Gavasso</h1>
+//             <p className="job-description">Frontend Developer</p>
+//             <small className="web-site"><a href="https://rokuogun.github.io/" target="_blank">ruokuogun.github.io</a></small>
+//         </header>
+//     )
+// }
 
 function Nav() {
     return (
