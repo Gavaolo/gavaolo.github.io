164c92a2bc8d9f912587b163a475d4590d79fd64
Paolo Gavasso <82797405+RokuoGun@users.noreply.github.com>
2022-04-05 07:58:11 +0200

    Update Header.js

--- projects/page_template/digital_business_card/Header.js
+++ projects/page_template/digital_business_card/Header.js
@@ -3,7 +3,7 @@ export default function Header() {
     return (
         <header>
             <img src="./img/profile_photo.jpg" />
-            <h1 className="card-name">Paolo Gavasso</h1>
+            <h1 className="card-name">Paolo Gavasso TEST IMPORT</h1>
             <p className="job-description">Frontend Developer</p>
             <small className="web-site"><a href="https://rokuogun.github.io/" target="_blank">ruokuogun.github.io</a></small>
         </header>
