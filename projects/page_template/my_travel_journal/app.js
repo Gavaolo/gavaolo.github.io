86e70f4dcd9561197e77cda21f2cb7b4b19bcee9
RokuoGun <gavassopaolo@gmail.com>
2022-04-06 21:29:57 +0200

    mod

--- projects/page_template/my_travel_journal/app.js
+++ projects/page_template/my_travel_journal/app.js
@@ -1,6 +1,28 @@
+function Navbar() {
+    return(
+        <nav>
+            <span><span class="fas fa-globe-americas"></span> my travel journal.</span>
+        </nav>
+    )
+}
+
+function Travel() {
+    return (
+        <div>
+            <img src=""/>
+            <div className="travel-content">
+                
+            </div>
+        </div>
+    )
+}
+
 function App() {
     return (
-        <div>Hello!</div>
+        <div className="container">
+            <Navbar />
+            <Main />
+        </div>
     )
 }
 
