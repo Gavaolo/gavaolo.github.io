0e82ff28e4ba508c36c347d59a67cc026e25595e
RokuoGun <gavassopaolo@gmail.com>
2022-04-08 07:51:43 +0200

    mod

--- projects/page_template/my_travel_journal/app.js
+++ projects/page_template/my_travel_journal/app.js
@@ -19,23 +19,23 @@ const data = [
         img: "img/sydney_opera_house.png",
         country: "AUSTRALIA",
         link: "https://www.google.com/maps/place/Teatro+dell'opera+di+Sydney/@-33.8567799,151.213108,17z/data=!3m2!4b1!5s0x6b12ae67d234a27f:0xd6d4e9380ca1e32f!4m5!3m4!1s0x6b12ae665e892fdd:0x3133f8d75a1ac251!8m2!3d-33.8567844!4d151.2152967",
-        title: "Mount Fuji",
-        date: "12 Jan, 2021 -24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists",
+        title: "Sydney Opera House",
+        date: "27 May, 2021 - 8 Jun, 2021",
+        description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
     },
     {
         img: "img/geirangerfjord.png",
-        country: "JAPAN",
+        country: "NORWAY",
         link: "https://www.google.com/maps/place/Geirangerfjord/@62.1049385,6.9351327,11z/data=!3m1!4b1!4m5!3m4!1s0x46169d427b268c51:0xb8c99540dcc397fe!8m2!3d62.101506!4d7.0940816",
-        title: "Mount Fuji",
-        date: "12 Jan, 2021 -24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists",
+        title: "Geirangerfjord",
+        date: "01 Oct, 2021 - 18 Nov, 2021",
+        description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
     }
 ]
 
 function Travel(props) {
     return (
-        <div className="travel">
+        <div>
             <div className="travel-info">
                 <img src={props.img} className="travel-image"/>
                 <div className="travel-content">
@@ -57,7 +57,7 @@ function Travel(props) {
 function App() {
     const travels = data.map(item => {
         return (
-            <Travel 
+            <Travel
                 key = {item.id}
                 {...item}
             />
@@ -67,7 +67,10 @@ function App() {
     return (
         <div className="container">
             <Navbar />
-            {travels}
+            <div className="travels">
+                {travels}
+            </div>
+            
         </div>
     )
 }
