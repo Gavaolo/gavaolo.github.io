bd35c34c0cbe9b7661a6d596e68be06eafc4f71b
RokuoGun <gavassopaolo@gmail.com>
2022-06-06 15:39:31 +0200

    mod

--- projects/tools/25+5_clock/app.js
+++ projects/tools/25+5_clock/app.js
@@ -1,20 +1,3 @@
-/* global React, ReactDOM */
-/* eslint-disable react/no-multi-comp, react/prop-types, max-len */
-
-/** NOTES:
-/** Dependencies are React, ReactDOM, and
-    Accurate_Interval.js by Squuege (external script
-    to keep setInterval() from drifting over time &
-    thus ensuring timer goes off at correct mark).
-/** Utilizes embedded <Audio> tag to ensure audio
-    plays when timer tab is inactive or browser is
-    minimized ( rather than new Audio().play() ).
-/** Audio of this fashion not supported on most
-    mobile devices it would seem (bummer I know).
-**/
-
-// PROJECTOR SELECTOR FOR EXTERNAL TEST SCRIPT:
-// eslint-disable-next-line no-unused-vars
 const projectName = '25-5-clock';
 
 const accurateInterval = function (fn, time) {
